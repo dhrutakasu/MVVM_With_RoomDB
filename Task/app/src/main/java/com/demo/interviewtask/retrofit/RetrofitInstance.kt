d0f947e8ed5528b240b3ae5cc61f01903fddce94
LENOVO <dhrutakasodariya217@gmail.com>
2023-03-06 23:46:21 +0530

    add new link

--- Task/app/src/main/java/com/demo/interviewtask/retrofit/RetrofitInstance.kt
+++ Task/app/src/main/java/com/demo/interviewtask/retrofit/RetrofitInstance.kt
@@ -3,6 +3,7 @@ package com.demo.interviewtask.retrofit
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
+//    https://run.mocky.io/v3/4fe37a6e-3d88-4ec9-af04-759917b8b9dc
     val api : DataApi by lazy {
         Retrofit.Builder()
             .baseUrl("https://run.mocky.io/v3/")
